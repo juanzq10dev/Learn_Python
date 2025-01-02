b3f7d37a25f2a4bab5a8340b0c5a15b0bf12d15e
Juan Zurita <juanzq10dev@gmail.com>
2025-01-02 13:32:55 -0400

    POKEMON: Add pokemon trainers

--- reference-vault/projects/pokemon/src/app/game/battle.py
+++ reference-vault/projects/pokemon/src/app/game/battle.py
@@ -1,23 +1,30 @@
 import app.ui.messages as logs
 import app.ui.io as io
 from app.lib.pokemon import Pokemon
+from app.game.trainer import Pokemon_Trainer
 
 
 class Battle:
     def __init__(self):
-        self.player1 = Pokemon("Player1", 100)
-        self.player2 = Pokemon("Player2", 100)
-        self.players_turn = self.player2
-        self.not_players_turn = self.player1
+        self.trainer1 = Pokemon_Trainer(
+            "Player 1", [Pokemon("Pikachu", 100), Pokemon("Raychu", 200)]
+        )
+        self.trainer2 = Pokemon_Trainer(
+            "Player 2", [Pokemon("Charizard", 100), Pokemon("Bulbasaur", 200)]
+        )
+        self.pokemon1 = self.trainer1.pokemon_team[0]
+        self.pokemon2 = self.trainer2.pokemon_team[0]
+        self.players_turn = self.pokemon2
+        self.not_players_turn = self.pokemon1
         pass
 
     def change_turn(self):
-        if self.players_turn == self.player1:
-            self.players_turn = self.player2
-            self.not_players_turn = self.player1
+        if self.players_turn == self.pokemon1:
+            self.players_turn = self.pokemon2
+            self.not_players_turn = self.pokemon1
         else:
-            self.players_turn = self.player1
-            self.not_players_turn = self.player2
+            self.players_turn = self.pokemon1
+            self.not_players_turn = self.pokemon2
 
     def game_loop(self):
         game_finished = False
@@ -30,19 +37,19 @@ class Battle:
         self.show_health_bars()
 
     def check_game_finished(self) -> bool:
-        if self.player1.hp <= 0:
-            logs.show_winner(self.player2.name)
+        if self.pokemon1.hp <= 0:
+            logs.show_winner(self.pokemon2.name)
             return True
 
-        if self.player2.hp <= 0:
-            logs.show_winner(self.player1.name)
+        if self.pokemon2.hp <= 0:
+            logs.show_winner(self.pokemon1.name)
             return True
 
         return False
 
     def action(self):
         logs.choose_action(self.players_turn.name)
-        
+
         user_input_int = io.read_input_int("Select action: ")
 
         if user_input_int is None:
@@ -59,5 +66,5 @@ class Battle:
                 self.action()
 
     def show_health_bars(self):
-        logs.show_pokemon_healthbar(self.player1)
-        logs.show_pokemon_healthbar(self.player2)
+        logs.show_pokemon_healthbar(self.pokemon1)
+        logs.show_pokemon_healthbar(self.pokemon2)
