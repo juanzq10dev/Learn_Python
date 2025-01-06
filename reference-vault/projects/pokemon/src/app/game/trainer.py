d9c6db5248bedcf6bc6329093f08060c72995316
Juan Zurita <juanzq10dev@gmail.com>
2025-01-06 16:05:35 -0400

    POKEMON: Move io functionality to trainer

--- reference-vault/projects/pokemon/src/app/game/trainer.py
+++ reference-vault/projects/pokemon/src/app/game/trainer.py
@@ -8,13 +8,32 @@ class Pokemon_Trainer:
     def __init__(self, name: str, pokemon_team: List["Pokemon"]):
         self.name = name
         self.pokemon_team = pokemon_team
-        self.pokemon_in_battle = pokemon_team[0]
+        self.pokemon_in_battle_index = 0
 
-    def choose_pokemon(self) -> "Pokemon":
+    def get_pokemon_in_battle(self) -> "Pokemon":
+        return self.pokemon_team[self.pokemon_in_battle_index]
+
+    def choose_attack(self) -> int:
+        user_input_int = io.read_input_int("Select an option")
+        if (
+            user_input_int is None
+            or user_input_int < 0
+            or user_input_int >= len(self.get_pokemon_in_battle().moves)
+        ):
+            self.choose_attack()
+        
+        return user_input_int
+
+    def choose_pokemon(self) -> None:
         display_messages.select_pokemon(self.pokemon_team)
         user_input_int = io.read_input_int("Select action: ")
 
-        if user_input_int < 0 or user_input_int > len(self.pokemon_team):
+        if (
+            user_input_int is None
+            or user_input_int < 0
+            or user_input_int >= len(self.pokemon_team)
+            or user_input_int == self.pokemon_in_battle_index
+        ):
             self.choose_pokemon()
 
-        return self.pokemon_team[user_input_int]
+        self.pokemon_in_battle_index = user_input_int
